0457e816aa9537d9a9a7f826535ab97a54ec6400
劳伟臻 <1149243551@qq.com>
2023-09-04 15:24:57 +0800

    bff层框架搭建

--- bff/request/auth.go
+++ bff/request/auth.go
@@ -1,7 +1,10 @@
 package request
 
+// 使用参考：https://gin-gonic.com/zh-cn/docs/examples/binding-and-validation/
+// 支持的规则：https://pkg.go.dev/github.com/go-playground/validator/v10#section-readme
+
 type Auth struct {
-	Phone    string `form:"phone" json:"phone" xml:"phone"  binding:"required"`
-	SmsCode  string `form:"sms_code" json:"sms_code" xml:"sms_code"  binding:"required"`
-	Password string `form:"password" json:"password" xml:"password"  binding:"required"`
+	Phone    string `form:"phone" json:"phone" xml:"phone"  binding:"required,len:11"`
+	SmsCode  uint32 `form:"sms_code" json:"sms_code" xml:"sms_code"  binding:"required,len:6"`
+	Password string `form:"password" json:"password" xml:"password"  binding:"required,min=6"`
 }
