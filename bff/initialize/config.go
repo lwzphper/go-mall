652bbb92a2c369b40ecca4f6d03905fc387cbdc0
劳伟臻 <1149243551@qq.com>
2023-10-16 18:30:47 +0800

    配置文件

--- bff/initialize/config.go
+++ bff/initialize/config.go
@@ -1,6 +1,7 @@
 package initialize
 
 import (
+	"flag"
 	"fmt"
 	"github.com/lwzphper/go-mall/bff/config"
 	"github.com/lwzphper/go-mall/bff/global"
@@ -9,8 +10,11 @@ import (
 
 // InitConfig 初始化配置文件
 func InitConfig() {
+	conf := flag.String("c", "bff/etc/config.yaml", "配置文件")
+	flag.Parse()
+
 	cfg := config.NewDefaultConfig()
-	err := cfgHelper.LoadConfigFromYml("bff/etc/config.yaml", cfg)
+	err := cfgHelper.LoadConfigFromYml(*conf, cfg)
 	if err != nil {
 		panic(fmt.Sprintf("load config from env error:%v", err))
 	}
--- server/member/initialize/config.go
+++ server/member/initialize/config.go
@@ -1,6 +1,7 @@
 package initialize
 
 import (
+	"flag"
 	"fmt"
 	cfgHelper "github.com/lwzphper/go-mall/pkg/config"
 	"github.com/lwzphper/go-mall/server/member/config"
@@ -9,8 +10,11 @@ import (
 
 // InitConfig 初始化配置文件
 func InitConfig() {
+	conf := flag.String("c", "admin/etc/config.yaml", "配置文件")
+	flag.Parse()
+
 	cfg := config.NewDefaultConfig()
-	err := cfgHelper.LoadConfigFromYml("server/member/etc/config.yaml", cfg)
+	err := cfgHelper.LoadConfigFromYml(*conf, cfg)
 	if err != nil {
 		panic(fmt.Sprintf("load config from env error:%v", err))
 	}
