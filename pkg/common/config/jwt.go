cd0632b9d4ab0c769bf0adb5fbcd48fb30183da2
劳伟臻 <1149243551@qq.com>
2023-10-19 17:12:05 +0800

    jwt配置文件调整

--- pkg/common/config/jwt.go
+++ pkg/common/config/jwt.go
@@ -3,8 +3,8 @@ package config
 import "time"
 
 type Jwt struct {
-	Secret string        `toml:"secret" yaml:"secret" mapstructure:"host" env:"JWT_SECRET"`
-	TTL    time.Duration `toml:"ttl" yaml:"ttl" mapstructure:"host" env:"JWT_TTL"`
+	Secret string        `toml:"secret" yaml:"secret" mapstructure:"secret" env:"JWT_SECRET"`
+	TTL    time.Duration `toml:"ttl" yaml:"ttl" mapstructure:"ttl" env:"JWT_TTL"`
 }
 
 func NewJwt() *Jwt {
